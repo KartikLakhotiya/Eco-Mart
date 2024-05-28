e6990b770042f6dd8b11a1268a751d8a4d5e2fbf
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-05-29 00:57:07 +0530

    add production and get product functions defined

--- src/App.jsx
+++ src/App.jsx
@@ -71,9 +71,9 @@ export const ProtectedRoutes = ({ children }) => {
 }
 
 export const ProtectedRoutesForAdmin = ({children}) => {
-  const admin = JSON.parse(localStorage.getItem('user'))
-  console.log(admin.user.email)
-  if (admin.user.email === 'admin@gmail.com') {
+  const result = JSON.parse(localStorage.getItem('user'))
+  // console.log('a',admin)
+  if (result) {
     return children
   }
   else {
