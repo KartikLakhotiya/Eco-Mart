3f0fca5059a0e94acb4f1dfdc644a939bb306f77
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-06-12 11:52:31 +0530

    add products, orders, users count on admin dashboard

--- src/pages/admin/dashboard/Dashboard.jsx
+++ src/pages/admin/dashboard/Dashboard.jsx
@@ -1,12 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FaUserTie } from "react-icons/fa";
 import myContext from "../../../context/data/myContext";
 import Layout from "../../../components/layout/Layout";
 import DashboardTab from "./DashboardTab";
-
+import { firestore } from "../../../firebase/FirebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
 function Dashboard() {
   const context = useContext(myContext);
   const { mode } = context;
+  const [productCount, setProductCount] = useState(0);
+  const [ordersCount, setOrdersCount] = useState(0);
+  const [usersCount, setUsersCount] = useState(0);
+
+  const fetchCount = async () => {
+
+    // Products Doc
+    const productsCollection = collection(firestore, 'products')
+    const productsSnapshot = await getDocs(productsCollection);
+    const productCount = productsSnapshot.size;
+    console.log(productCount)
+    setProductCount(productCount);
+
+    // Orders Doc
+    const ordersCollection = collection(firestore, 'orders')
+    const ordersSnapshot = await getDocs(ordersCollection)
+    const ordersCount = ordersSnapshot.size;
+    setOrdersCount(ordersCount);
+
+    // Users Doc
+    const usersCollection = collection(firestore, 'users')
+    const usersSnapshot = await getDocs(usersCollection)
+    const usersCount = usersSnapshot.size;
+    setUsersCount(usersCount);
+
+
+
+  }
+  fetchCount();
+
   return (
     <Layout>
       <section className="text-gray-600 body-font mt-10 mb-10 m-[auto]">
@@ -19,7 +50,7 @@ function Dashboard() {
                 </div>
                 <p className="font-normal text-gray-700 dark:text-gray-400">
                   <h2 className="text-[20px] text-black">
-                    10
+                    {productCount}
                     <p>Total Products</p>
                   </h2>
                 </p>
@@ -32,7 +63,7 @@ function Dashboard() {
                 </div>
                 <p className="font-normal text-gray-700 dark:text-gray-400">
                   <h2 className="text-[20px] text-black">
-                    10
+                    {ordersCount}
                     <p>Total Orders</p>
                   </h2>
                 </p>
@@ -45,7 +76,7 @@ function Dashboard() {
                 </div>
                 <p className="font-normal text-gray-700 dark:text-gray-400">
                   <h2 className="text-[20px] text-black">
-                    10
+                    {usersCount}
                     <p>Total Users</p>
                   </h2>
                 </p>
